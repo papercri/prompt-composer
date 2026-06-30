e399287f4cc349612ad190bfa69a3f5696139ac0
Your NameCristiana <papercri@gmail.com>
2026-06-30 18:12:31 +0200

    footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,15 +4,15 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="w-full bg-linear-to-r from-(--foreground) via-(--secondary) to-(--primary) text-slate-200 text-sm text-center py-3 shrink-0 shadow-inner">
-      © {new Date().getFullYear()} Prompt Composer by{" "}
-      <a
-        href="https://www.linkedin.com/in/cristianasollini/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-(--rose)  hover:text-white transition-colors ml-1"
-      >
-        @papercri
-      </a>
+          © {new Date().getFullYear()} Prompt Composer by{" "}
+          <a
+            href="https://frontend-ux.website/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-(--rose) hover:text-white transition-colors"
+          >
+            Cristiana Sollini
+          </a>
     </footer>
   );
 }
